a890c1084023d98b17ba8c88e3fcd24e9cdf7686
Andy Truong <att35663@uga.edu>
2018-07-04 11:27:29 -0400

    Updated API Calls to V2

--- test.js
+++ test.js
@@ -13,11 +13,11 @@ function calculateTotal() {
         if (this.readyState == 4 && this.status == 200) 
         {
         var myArr = JSON.parse(this.responseText);
-        var price = document.getElementById("bitcoinPrice").innerHTML = Number(myArr[0].price_usd).toFixed(2);
+        var price = document.getElementById("bitcoinPrice").innerHTML = Number(myArr.data.quotes.USD.price).toFixed(2);
         document.getElementById("bitcoinTotalAmount").innerHTML = (price * 0).toFixed(2);   
         }
     };
-        xmlhttp.open("GET", "https://api.coinmarketcap.com/v1/ticker/bitcoin/", true);
+        xmlhttp.open("GET", "https://api.coinmarketcap.com/v2/ticker/1/", true);
         xmlhttp.send();
 
         var xmlhttp2 = new XMLHttpRequest();
@@ -26,14 +26,14 @@ function calculateTotal() {
             if (this.readyState == 4 && this.status == 200) 
             {
             var myArr = JSON.parse(this.responseText);
-            var price = document.getElementById("neoPrice").innerHTML = Number(myArr[0].price_usd).toFixed(2); 
+            var price = document.getElementById("neoPrice").innerHTML = Number(myArr.data.quotes.USD.price).toFixed(2); 
             document.getElementById("neoPrice").innerHTML = price;
             var totalAmount = document.getElementById("neoTotalAmount").innerHTML = (price * 5).toFixed(2);
             num = parseInt(num) + parseInt(totalAmount);
            
             }
         };
-        xmlhttp2.open("GET", "https://api.coinmarketcap.com/v1/ticker/neo/", true);
+        xmlhttp2.open("GET", "https://api.coinmarketcap.com/v2/ticker/1376/", true);
         xmlhttp2.send();
         
         var xmlhttp3 = new XMLHttpRequest();
@@ -42,13 +42,13 @@ function calculateTotal() {
             if (this.readyState == 4 && this.status == 200) 
             {
             var myArr = JSON.parse(this.responseText);
-            var price = document.getElementById("iotaPrice").innerHTML = Number(myArr[0].price_usd).toFixed(2);
+            var price = document.getElementById("iotaPrice").innerHTML = Number(myArr.data.quotes.USD.price).toFixed(2);
             var totalAmount = document.getElementById("iotaTotalAmount").innerHTML = (price * 5).toFixed(2);
             num = parseInt(num) + parseInt(totalAmount);
             
             }
         };
-        xmlhttp3.open("GET", "https://api.coinmarketcap.com/v1/ticker/iota/", true);
+        xmlhttp3.open("GET", "https://api.coinmarketcap.com/v2/ticker/1720/", true);
         xmlhttp3.send();
 
         var xmlhttp4 = new XMLHttpRequest();
@@ -57,12 +57,12 @@ function calculateTotal() {
             if (this.readyState == 4 && this.status == 200) 
             {
             var myArr = JSON.parse(this.responseText);
-            var price = document.getElementById("vertcoinPrice").innerHTML = Number(myArr[0].price_usd).toFixed(2);
+            var price = document.getElementById("vertcoinPrice").innerHTML = Number(myArr.data.quotes.USD.price).toFixed(2);
             var totalAmount = document.getElementById("vertcoinTotalAmount").innerHTML = (price * 5).toFixed(2);
             num = parseInt(num) + parseInt(totalAmount);
             }
         };
-        xmlhttp4.open("GET", "https://api.coinmarketcap.com/v1/ticker/vertcoin/", true);
+        xmlhttp4.open("GET", "https://api.coinmarketcap.com/v2/ticker/99/", true);
         xmlhttp4.send();
 
 
@@ -72,13 +72,13 @@ function calculateTotal() {
             if (this.readyState == 4 && this.status == 200) 
             {
             var myArr = JSON.parse(this.responseText);
-            var price = document.getElementById("carnadoPrice").innerHTML = Number(myArr[0].price_usd).toFixed(2);
+            var price = document.getElementById("carnadoPrice").innerHTML = Number(myArr.data.quotes.USD.price).toFixed(2);
             var totalAmount = document.getElementById("carnadoTotalAmount").innerHTML = (price * 5).toFixed(2);
             num = parseInt(num) + parseInt(totalAmount);
             
             }
         };
-        xmlhttp5.open("GET", "https://api.coinmarketcap.com/v1/ticker/cardano/", true);
+        xmlhttp5.open("GET", "https://api.coinmarketcap.com/v2/ticker/2010/", true);
         xmlhttp5.send();
 
         var xmlhttp6 = new XMLHttpRequest();
@@ -87,12 +87,12 @@ function calculateTotal() {
             if (this.readyState == 4 && this.status == 200) 
             {
             var myArr = JSON.parse(this.responseText);
-            var price = document.getElementById("golemPrice").innerHTML = Number(myArr[0].price_usd).toFixed(2);
+            var price = document.getElementById("golemPrice").innerHTML = Number(myArr.data.quotes.USD.price).toFixed(2);
             var totalAmount = document.getElementById("golemTotalAmount").innerHTML = (price * 5).toFixed(2);
             num = parseInt(num) + parseInt(totalAmount);
             }
         };
-        xmlhttp6.open("GET", "https://api.coinmarketcap.com/v1/ticker/golem-network-tokens/", true);
+        xmlhttp6.open("GET", "https://api.coinmarketcap.com/v2/ticker/1455/", true);
         xmlhttp6.send();
         
         var xmlhttp7 = new XMLHttpRequest();
@@ -101,12 +101,12 @@ function calculateTotal() {
             if (this.readyState == 4 && this.status == 200) 
             {
             var myArr = JSON.parse(this.responseText);
-            var price = document.getElementById("arkPrice").innerHTML = Number(myArr[0].price_usd).toFixed(2);
+            var price = document.getElementById("arkPrice").innerHTML = Number(myArr.data.quotes.USD.price).toFixed(2);
             var totalAmount = document.getElementById("arkTotalAmount").innerHTML = (price * 5).toFixed(2);
             num = parseInt(num) + parseInt(totalAmount);
             }
         };
-        xmlhttp7.open("GET", "https://api.coinmarketcap.com/v1/ticker/ark/", true);
+        xmlhttp7.open("GET", "https://api.coinmarketcap.com/v2/ticker/1586/", true);
         xmlhttp7.send();
        
         
@@ -116,12 +116,12 @@ function calculateTotal() {
             if (this.readyState == 4 && this.status == 200) 
             {
             var myArr = JSON.parse(this.responseText);
-            var price = document.getElementById("substratumPrice").innerHTML = Number(myArr[0].price_usd).toFixed(2);
+            var price = document.getElementById("substratumPrice").innerHTML = Number(myArr.data.quotes.USD.price).toFixed(2);
             var totalAmount = document.getElementById("substratumTotalAmount").innerHTML = (price * 5).toFixed(2);
             num = parseInt(num) + parseInt(totalAmount);
             }
         }
-        xmlhttp13.open("GET", "https://api.coinmarketcap.com/v1/ticker/substratum/", true);
+        xmlhttp13.open("GET", "https://api.coinmarketcap.com/v2/ticker/1984/", true);
         xmlhttp13.send();
         
         var xmlhttp14 = new XMLHttpRequest();
@@ -130,12 +130,12 @@ function calculateTotal() {
             if (this.readyState == 4 && this.status == 200) 
             {
             var myArr = JSON.parse(this.responseText);
-            var price = document.getElementById("requestNetworkPrice").innerHTML = Number(myArr[0].price_usd).toFixed(2);
+            var price = document.getElementById("requestNetworkPrice").innerHTML = Number(myArr.data.quotes.USD.price).toFixed(2);
             var totalAmount = document.getElementById("requestNetworkTotalAmount").innerHTML = (price * 5).toFixed(2);
             num = parseInt(num) + parseInt(totalAmount);
             }
         }
-        xmlhttp14.open("GET", "https://api.coinmarketcap.com/v1/ticker/request-network/", true);
+        xmlhttp14.open("GET", "https://api.coinmarketcap.com/v2/ticker/2071/", true);
         xmlhttp14.send();
         
         var xmlhttp15 = new XMLHttpRequest();
@@ -144,12 +144,12 @@ function calculateTotal() {
             if (this.readyState == 4 && this.status == 200) 
             {
             var myArr = JSON.parse(this.responseText);
-            var price = document.getElementById("wabiPrice").innerHTML = Number(myArr[0].price_usd).toFixed(2);
+            var price = document.getElementById("wabiPrice").innerHTML = Number(myArr.data.quotes.USD.price).toFixed(2);
             var totalAmount = document.getElementById("wabiTotalAmount").innerHTML = (price * 5).toFixed(2);
             num = parseInt(num) + parseInt(totalAmount);   
             }
         }
-        xmlhttp15.open("GET", "https://api.coinmarketcap.com/v1/ticker/wabi/", true);
+        xmlhttp15.open("GET", "https://api.coinmarketcap.com/v2/ticker/2267/", true);
         xmlhttp15.send();
         
         var xmlhttp16 = new XMLHttpRequest();
@@ -158,12 +158,12 @@ function calculateTotal() {
             if (this.readyState == 4 && this.status == 200) 
             {
             var myArr = JSON.parse(this.responseText);
-            var price = document.getElementById("stellarLumensPrice").innerHTML = Number(myArr[0].price_usd).toFixed(2);
+            var price = document.getElementById("stellarLumensPrice").innerHTML = Number(myArr.data.quotes.USD.price).toFixed(2);
             var totalAmount = document.getElementById("stellarLumensTotalAmount").innerHTML = (price * 5).toFixed(2); 
             num = parseInt(num) + parseInt(totalAmount);
             }
         }
-        xmlhttp16.open("GET", "https://api.coinmarketcap.com/v1/ticker/stellar/", true);
+        xmlhttp16.open("GET", "https://api.coinmarketcap.com/v2/ticker/512/", true);
         xmlhttp16.send();
         
         var xmlhttp17 = new XMLHttpRequest();
@@ -172,12 +172,12 @@ function calculateTotal() {
             if (this.readyState == 4 && this.status == 200) 
             {
             var myArr = JSON.parse(this.responseText);
-            var price = document.getElementById("iconPrice").innerHTML = Number(myArr[0].price_usd).toFixed(2);
+            var price = document.getElementById("iconPrice").innerHTML = Number(myArr.data.quotes.USD.price).toFixed(2);
             var totalAmount = document.getElementById("iconTotalAmount").innerHTML = (price * 5).toFixed(2);
             num = parseInt(num) + parseInt(totalAmount);    
             }
         }
-        xmlhttp17.open("GET", "https://api.coinmarketcap.com/v1/ticker/icon/", true);
+        xmlhttp17.open("GET", "https://api.coinmarketcap.com/v2/ticker/2099/", true);
         xmlhttp17.send();
         
         
@@ -191,11 +191,11 @@ function calculateTotal() {
             if (this.readyState == 4 && this.status == 200) 
             {
             var myArr = JSON.parse(this.responseText);
-            var price = document.getElementById("ripplePrice").innerHTML = Number(myArr[0].price_usd).toFixed(2);
+            var price = document.getElementById("ripplePrice").innerHTML = Number(myArr.data.quotes.USD.price).toFixed(2);
             var totalAmount = document.getElementById("rippleTotalAmount").innerHTML = (price * 5).toFixed(2);
             num = parseInt(num) + parseInt(totalAmount);
             }
         }
-        xmlhttp20.open("GET", "https://api.coinmarketcap.com/v1/ticker/ripple/", true);
+        xmlhttp20.open("GET", "https://api.coinmarketcap.com/v2/ticker/52/", true);
         xmlhttp20.send();
         
