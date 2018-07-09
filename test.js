3f61d693a73685ce4bf3961dd89317e88975d8c7
Andy Truong <att35663@uga.edu>
2018-07-08 23:27:40 -0400

    Reducing code in javascript file

--- test.js
+++ test.js
@@ -3,9 +3,27 @@
 function calculateTotal() {
     document.getElementById("totalAmount").innerHTML = num;
 }
+        
+        var app = angular.module('myApp', []);
 
 
         var num = 0;
+
+        app.controller('myCtrl', function($scope, $http, $timeout) {
+        
+        
+        
+        /*
+        * Request data for top 20 cryptocurrencies by total 
+        * marketcap from CoinMarketCap API
+        */
+        $http.get("https://api.coinmarketcap.com/v2/ticker/?limit=20&sort=rank&structure=array")
+            .then(function(myArr) {
+            $scope.myWelcome = myArr;
+        });
+    
+        
+    });
 
         var xmlhttp = new XMLHttpRequest();
         xmlhttp.onreadystatechange = function() 
