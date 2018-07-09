b18e789d8fcca1140ce63c7beed5168c1d4b4f76
Andy Truong <att35663@uga.edu>
2018-07-09 15:18:31 -0400

    Added search bar

--- test.js
+++ test.js
@@ -17,7 +17,7 @@ function calculateTotal() {
         * Request data for top 20 cryptocurrencies by total 
         * marketcap from CoinMarketCap API
         */
-        $http.get("https://api.coinmarketcap.com/v2/ticker/?limit=20&sort=rank&structure=array")
+        $http.get("https://api.coinmarketcap.com/v2/ticker/?limit=50&sort=rank&structure=array")
             .then(function(myArr) {
             $scope.myWelcome = myArr;
         });
